f7128d725f6d137fb80dcf7463f00247cf0c2122
Barry Myburgh <5949436+bright-spark@users.noreply.github.com>
2023-06-22 00:59:52 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -39,14 +39,14 @@ updateIframeSource();
 openPopupButton.addEventListener('click', () => {
 const websiteUrl = 'https://bio.to/navigator'; // Replace with the desired website URL
 iframe.src = websiteUrl;
-openPopupButton.classList.add('hidden');
-closePopupButton.classList.remove('hidden');
+//openPopupButton.classList.add('hidden');
+//closePopupButton.classList.remove('hidden');
 });
 
 // Close the popup
 closePopupButton.addEventListener('click', () => {
 iframe.src = 'https://bio.to/navigator'; // Clear the iframe source
-openPopupButton.classList.remove('hidden');
-closePopupButton.classList.add('hidden');
+//openPopupButton.classList.remove('hidden');
+//closePopupButton.classList.add('hidden');
 });
 
