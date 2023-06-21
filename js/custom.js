345cdde77375c3511eac0d1163b70fbdb48fcd2c
Barry Myburgh <5949436+bright-spark@users.noreply.github.com>
2023-06-22 01:25:30 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,23 @@
 // Get necessary DOM elements
-const openPopupButton = document.getElementById('open-popup');
-const closePopupButton = document.getElementById('close-popup');
-const popupContainer = document.getElementById('popup-container');
-const popup = document.getElementById('popup');
+//const openPopupButton = document.getElementById('open-popup');
+//const closePopupButton = document.getElementById('close-popup');
+//const popupContainer = document.getElementById('popup-container');
+//const popup = document.getElementById('popup');
 const iframe = document.getElementById('website-iframe');
 
 // Open the popup with the specified website URL
 openPopupButton.addEventListener('click', () => {
-const websiteUrl = 'https://bio.to/navigator'; // Replace with the desired website URL
-iframe.src = websiteUrl;
-openPopupButton.classList.add('hidden');
-closePopupButton.classList.remove('hidden');
+const openUrl = 'https://bio.to/navigator'; // Replace with the desired website URL
+iframe.src = openUrl;
+//openPopupButton.classList.add('hidden');
+//closePopupButton.classList.remove('hidden');
 });
 
 // Close the popup
 closePopupButton.addEventListener('click', () => {
-iframe.src = 'https://bio.to/navigator'; // Clear the iframe source
-openPopupButton.classList.remove('hidden');
-closePopupButton.classList.add('hidden');
+const closeUrl = 'https://bio.to/navigator';
+iframe.src = closeUrl; // Clear the iframe source
+//openPopupButton.classList.remove('hidden');
+//closePopupButton.classList.add('hidden');
 });
 
