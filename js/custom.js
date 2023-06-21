a3d418e8dad944021e71f0e9d34f5cd2b8f01bd4
Barry Myburgh <5949436+bright-spark@users.noreply.github.com>
2023-06-22 00:54:43 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -37,13 +37,16 @@ updateIframeSource();
 
 // Open the popup with the specified website URL
 openPopupButton.addEventListener('click', () => {
-const websiteUrl = 'https://example.com'; // Replace with the desired website URL
+const websiteUrl = 'https://bio.to/navigator'; // Replace with the desired website URL
 iframe.src = websiteUrl;
+openPopupButton.classList.add('hidden');
+closePopupButton.classList.remove('hidden');
 });
 
 // Close the popup
 closePopupButton.addEventListener('click', () => {
-iframe.src = ''; // Clear the iframe source
-popup.classList.add('hidden');
+iframe.src = 'https://bio.to/navigator'; // Clear the iframe source
+openPopupButton.classList.remove('hidden');
+closePopupButton.classList.add('hidden');
 });
 
