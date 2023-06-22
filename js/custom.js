ac7857be3e5d7190ffe4842442592d45de1c3299
Barry Myburgh <5949436+bright-spark@users.noreply.github.com>
2023-06-22 02:59:41 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -7,15 +7,15 @@ const iframe = document.getElementById('website-iframe');
 
 // Open the popup with the specified website URL
 openPopupButton.addEventListener('click', () => {
-  const websiteUrl = 'https://external-navigator.carrd.co'; // Replace with the desired website URL
+  const websiteUrl = 'https://lnk.to/navigator'; // Replace with the desired website URL
   iframe.src = websiteUrl;
-  popup.classList.remove('hidden');
-  popupContainer.style.overflow = 'hidden'; // Prevent scrolling of the main page
+  //popup.classList.remove('hidden');
+  //popupContainer.style.overflow = 'hidden'; // Prevent scrolling of the main page
 });
 
 // Close the popup
 closePopupButton.addEventListener('click', () => {
-  iframe.src = ''; // Clear the iframe source
-  popup.classList.add('hidden');
-  popupContainer.style.overflow = 'auto'; // Restore scrolling of the main page
+  iframe.src = 'https://lnk.to/navigator'; // Clear the iframe source
+  //popup.classList.add('hidden');
+  //popupContainer.style.overflow = 'auto'; // Restore scrolling of the main page
 });
