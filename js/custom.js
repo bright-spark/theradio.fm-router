2f4c6c157a46c0070df8a64b474e2789796ed70d
Barry Myburgh <hex0h@outlook.com>
2023-06-22 05:36:07 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 // Get necessary DOM elements
 const openPopupButton = document.getElementById('open-popup');
-const closePopupButton = document.getElementById('close-popup');
-const popupContainer = document.getElementById('popup-container');
-const popup = document.getElementById('popup');
+//const closePopupButton = document.getElementById('close-popup');
+//const popupContainer = document.getElementById('popup-container');
+//const popup = document.getElementById('popup');
 const iframe = document.getElementById('website-iframe');
 
 // Open the iframe with the specified website URL
